35b9dab875c6bdab83824598b405e33164bc6b32
Stefan Broenner <stbrnner@microsoft.com>
2020-03-10 17:20:20 +0100

    First iteration of CosmosDB Integrtion

--- sbroennelab.nhkworldtv.Tests/Test_Vod.cs
+++ sbroennelab.nhkworldtv.Tests/Test_Vod.cs
@@ -3,7 +3,6 @@ using Xunit;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
-
 namespace sbroennelab.nhkworldtv.Tests
 {
     public class Test_Vod
@@ -12,8 +11,19 @@ namespace sbroennelab.nhkworldtv.Tests
         public async void Test_GetProgramUuid()
         {
             string vodId = "U1d2xiaDE6qTdDXmxFFeDzQgE4930P88";
-            var programUuid =  await sbroennelab.nhkworldtv.Vod.GetProgramUuid(vodId);
+            var programUuid = await sbroennelab.nhkworldtv.Vod.GetProgramUuid(vodId);
             Assert.NotEmpty(programUuid);
+        }
+
+        [Fact]
+        public async void Test_GetVodProgram()
+        {
+            string vodId = "U1d2xiaDE6qTdDXmxFFeDzQgE4930P88";
+            var programEntitiy = await sbroennelab.nhkworldtv.Vod.GetVodProgram(vodId);
+            
+            Assert.NotEmpty(programEntitiy.ProgramUuid);
+
         }
     }
 }
+
--- sbroennelab.nhkworldtv/Vod.cs
+++ sbroennelab.nhkworldtv/Vod.cs
@@ -10,14 +10,25 @@ using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.Azure.Cosmos.Table;
+
 
 namespace sbroennelab.nhkworldtv
 {
-    public class Program
+    public class ProgramEntity : TableEntity
     {
-       public string VodId { get; set; }  
-       public string ProgramUuid { get; set; }
-       public DateTime LastUpdatedUtc { get; set; }
+        public ProgramEntity()
+        {
+        }
+
+        public ProgramEntity(string vodId, string partitionKey)
+        {
+            PartitionKey = partitionKey;
+            RowKey = vodId;
+        }
+
+        public string ProgramUuid { get; set; }
+
     }
 
     public static class Vod
@@ -28,6 +39,53 @@ namespace sbroennelab.nhkworldtv
         private static Regex rx = new Regex(@"'data-de-program-uuid','(.+?)'",
               RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
+        private static string connectionString = "DefaultEndpointsProtocol=https;AccountName=nhkdb;AccountKey=WGDqURUL63TNF1WnqQKNXCmnupVu3eMCZeqEF0DpSrOZdpQq9sVGYEpPzDYLuCeMUCFB2eL9krjZfQ3C6zrHrg==;TableEndpoint=https://nhkdb.table.cosmos.azure.com:443/;";
+        private static CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+        private static CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+        private static CloudTable programTable = tableClient.GetTableReference("VodProgram");
+
+        private static string partitionKey = "default";
+
+        public static async Task<ProgramEntity> InsertProgramEntity(CloudTable table, ProgramEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            try
+            {
+                // Create the InsertOrReplace table operation
+                TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(entity);
+
+                // Execute the operation.
+                TableResult result = await table.ExecuteAsync(insertOrMergeOperation);
+                ProgramEntity insertedProgram = result.Result as ProgramEntity;
+                return insertedProgram;
+            }
+            catch (StorageException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+
+        public static async Task<ProgramEntity> RetrieveProgramEntitiy(CloudTable table, string partitionKey, string rowKey)
+        {
+            try
+            {
+                TableOperation retrieveOperation = TableOperation.Retrieve<ProgramEntity>(partitionKey, rowKey);
+                TableResult result = await table.ExecuteAsync(retrieveOperation);
+                ProgramEntity customer = result.Result as ProgramEntity;
+                return customer;
+            }
+            catch (StorageException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
         public static async Task<String> GetProgramUuid(string vodId)
         {
             var playerUrl = String.Format("https://movie-s.nhk.or.jp/v/refid/nhkworld/prefid/{0}?embed=js&targetId=videoplayer&de-responsive=true&de-callback-method=nwCustomCallback&de-appid={1}&de-subtitle-on=false", vodId, vodId);
@@ -44,22 +102,28 @@ namespace sbroennelab.nhkworldtv
             return (programUuid);
         }
 
-        public static async Task<Program> GetVodProgram(string vodId)
+        public static async Task<ProgramEntity> GetVodProgram(string vodId)
         {
 
-            var programUuid = await GetProgramUuid(vodId);
+            // Retrieve Program from CosmosDB
+            var vodProgram = await RetrieveProgramEntitiy(programTable, partitionKey, vodId);
+            if (vodProgram == null)
+            {
+                // Not ins CosmosDB Retrieve Program from HNK
+                var programUuid = await GetProgramUuid(vodId);
 
-            var vodProgram = new Program();
-            vodProgram.VodId = vodId;
-            vodProgram.ProgramUuid = programUuid;
-            vodProgram.LastUpdatedUtc = DateTime.UtcNow;
-
-            return(vodProgram);
+                // Store in CosmosDB
+                vodProgram = new ProgramEntity(vodId, partitionKey);
+                vodProgram.ProgramUuid = programUuid;
+                vodProgram = await InsertProgramEntity(programTable, vodProgram);
+            }
+            // Return the program
+            return (vodProgram);
         }
 
         [FunctionName("GetVodProgram")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route="Program/{vodId}")] HttpRequest req, string vodId,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Program/{vodId}")] HttpRequest req, string vodId,
             ILogger log)
         {
             var vodProgram = await GetVodProgram(vodId);
