9a917bf98d72106341496de97ce73fc8435113cd
sbroenne <stefan_broenner@yahoo.com>
2021-03-07 08:58:11 +0100

    Error handling for maxItems parameter

--- sbroennelab.nhkworldtv/GetVodProgramListV2.cs
+++ sbroennelab.nhkworldtv/GetVodProgramListV2.cs
@@ -17,15 +17,40 @@ namespace sbroennelab.nhkworldtv
         /// </summary>
         /// <param name="[HttpTrigger(AuthorizationLevel.Function">API Key</param>
         /// <param name=""Program/v2/List/{maxItems}"">Number of programs to return</param>
-        /// <returns>JSON with minimal information</returns>
+        /// <returns>JSON with minimal metadata information</returns>
         [FunctionName("GetVodListV2")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Program/v2/List/{maxItems}")] HttpRequest req, int maxItems,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Program/v2/List/{maxItems}")] HttpRequest req, string maxItems,
             ILogger log)
         {
-            string jsonString = await VodProgramList.GetProgramList(maxItems);
+            int items;
 
-            return new OkObjectResult(jsonString);
+            // Check if the maxItems parameter is an int
+            if (int.TryParse(maxItems, out items))
+            {
+                // Set sensible default value
+                if (items < 0) items = 1;
+
+                var jsonString = await VodProgramList.GetProgramList(items);
+                if (jsonString != null)
+                {
+                    // return ProgramList
+                    log.LogInformation("Executed GetProgramList({0}) - Size: {1}", items, jsonString.Length);
+                    return new OkObjectResult(jsonString);
+                }
+                else
+                {
+                    // No content - this should not really happen!
+                    log.LogError("No content returned for GetProgramList({0})", items);
+                    return new NoContentResult();
+                }
+            }
+            else
+            {
+                // Invalid maxItems parameter
+                log.LogInformation("Invalid maxItems parameter provided for GetProgramList: {0}", maxItems);
+                return new BadRequestResult();
+            }
         }
     }
 }
