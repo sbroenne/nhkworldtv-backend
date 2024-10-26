25ddced3170a721820e469aece637d0e641fb69d
sbroenne <stefan_broenner@yahoo.com>
2024-10-26 08:46:55 +0200

    Code clean-up

--- sbroennelab.nhkworldtv/TriggerUpdateBlob.cs
+++ sbroennelab.nhkworldtv/TriggerUpdateBlob.cs
@@ -1,6 +1,6 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker;
 
 namespace sbroennelab.nhkworldtv
 {
